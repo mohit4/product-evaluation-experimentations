6a5bcde431cdb47976cd6b80178ab09ae5252bef
mohit4 <mohitkumar2801@gmail.com>
2017-04-22 10:13:45 +0530

    added classifier and analysis scripts (incomplete)

---
+++ beta_2/naive_bayes_classifier.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+naive_bayes_classifier.py : used for training and testing the dataset
+
+input -
+    naive_bayes_classifier.py <dataset name> <train>:<test>
+
+output -
+    log file : naive_bayes_classifier_log.txt
+    saved classifier : nbc_<datetime>.pickle
+
+"""
+
+__author__ = "Mohit Kumar"
+
+dataset = "./dataset/kaggle_reviews.txt"
+
+training_ratio = 80
+
+reviews = []
+
+def fetch_reviews():
+
+
+import sys
+
+if __name__ == "__main__":
---
+++ beta_2/review_analysis.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""review_analysis.py : beta_2 code for review analysis"""
+
+__author__ = "Mohit Kumar"
+__credits__ = [""]
+__version__ = "0.9.2" # beta_2
+__maintainer__ = "Mohit Kumar"
+__email__ = "mohitkumar2801@gmail.com"
+__status__ = "Production"
