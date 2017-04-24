357827d5d6d40fa61674a4a5cf4b31d178b3352d
mohit4 <mohitkumar2801@gmail.com>
2017-04-24 18:58:05 +0530

    added experimented scripts

--- beta_2/naive_bayes_classifier.py
+++ beta_2/naive_bayes_classifier.py
@@ -20,8 +20,30 @@ training_ratio = 80
 
 reviews = []
 
-def fetch_reviews():
+import nltk, sys
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer as WNL
 
+def remove_non_ascii(word):
+    return ''.join(i for i in word if ord(i)<128)
+
+def lemmatize(list_of_words):
+    tagged_words = nltk.pos_tag(list_of_words)
+    for pair in tagged_words:
+
+
+def filter(text):
+    words = word_tokenize(text)
+    words = [remove_non_ascii(x) for x in words if len(x)>=3]
+    words =
+    return ''.join(words)
+
+def fetch_reviews():
+    fobj = open(dataset,'r')
+    while True:
+        text = fobj.readline()
+        label = 'pos' if text[0]=='1' else 'neg'
+        text = filter(text[2:])
 
 import sys
 
---
+++ scripts/demo.py
@@ -0,0 +1,77 @@
+"""
+This script is working...
+"""
+
+
+import nltk
+from nltk.tokenize import WordPunctTokenizer as WPT
+from nltk import NaiveBayesClassifier as NBC
+
+# word tokenizer to extract punctuations as words
+wpt = WPT()
+
+# path to dataset MUST be saved to the current directory
+path = './kaggle_reviews.txt'
+
+# dataset hold the list of words with label
+dataset = []
+
+# all words in current dataset
+all_words = []
+
+# set of feature words feeding to the classifier
+word_features = []
+
+# document feature extractor
+def document_features(list_of_words):
+    # global word_features
+    document_words = set(list_of_words)
+    features = {}
+    for word in word_features:
+        features['contains(%s)'%word] = (word in list_of_words)
+    return features
+
+if __name__ == "__main__":
+    # global dataset, all_words_set, word_features
+    fobj = open(path,'r')
+    while True:
+        text = fobj.readline()
+        if text == '':
+            break
+        label = 'pos' if text[0] == '1' else 'neg'
+
+        # tokenization, lower case, small words removed
+        text = [x.lower() for x in wpt.tokenize(text[2:]) if len(x)>=3]
+
+        # hint : remove proper nouns and unneccessary words to improve accuracy
+        # tagging
+        # lemmatization
+
+        # appending all words to the set
+        all_words.extend(text)
+        dataset.append((text,label))
+
+    print all_words
+
+    # now after getting the complete dataset
+    # we need to extract the top words with the highest frequency
+    word_features = nltk.FreqDist(all_words)
+
+    # now working with classifier
+    feature_set = [(document_features(d),c) for (d,c) in dataset]
+
+    # taking 100 entries for testing, rest are for training
+    train_set, test_set = feature_set[100:], feature_set[:100]
+
+    # now the naive bayes classifier
+    classifier = NBC.train(train_set)
+
+    # testing
+    acc = nltk.classify.accuracy(classifier, test_set)
+    print 'Accuracy :',acc
+
+    # most informative features
+    classifier.show_most_informative_features(10)
+
+    # done
+    print "Done!"
