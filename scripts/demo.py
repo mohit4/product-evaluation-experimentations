864b486c2e7b2bd0766b693aa78a0f3ee75cf690
mohit4 <mohitkumar2801@gmail.com>
2017-04-24 19:38:42 +0530

    fixed some bugs and improvements

--- scripts/demo.py
+++ scripts/demo.py
@@ -2,7 +2,6 @@
 This script is working...
 """
 
-
 import nltk
 from nltk.tokenize import WordPunctTokenizer as WPT
 from nltk import NaiveBayesClassifier as NBC
@@ -21,6 +20,17 @@ all_words = []
 
 # set of feature words feeding to the classifier
 word_features = []
+
+# filtering out the unneccessary words
+def feature_filter(list_of_words):
+    # acceptable = ['CC','DT','IN','JJ','JJR','JJS','MD','NNS','PDT','RB','RBR','RBS','RP','UH','VB','VBD','VBG','VBN','VBP','VBZ']
+    acceptable = ['JJ','JJR','JJS','RB','RBR','RBS','RP','VB','VBD','VBG','VBN','VBP','VBZ']
+    resulting_words = []
+    tagged_words = nltk.pos_tag(list_of_words)
+    for p in tagged_words:
+        if p[1] in acceptable:
+            resulting_words.append(p[0])
+    return resulting_words
 
 # document feature extractor
 def document_features(list_of_words):
@@ -41,7 +51,7 @@ if __name__ == "__main__":
         label = 'pos' if text[0] == '1' else 'neg'
 
         # tokenization, lower case, small words removed
-        text = [x.lower() for x in wpt.tokenize(text[2:]) if len(x)>=3]
+        text = feature_filter([x.lower() for x in wpt.tokenize(text[2:]) if len(x)>=3])
 
         # hint : remove proper nouns and unneccessary words to improve accuracy
         # tagging
@@ -55,7 +65,7 @@ if __name__ == "__main__":
 
     # now after getting the complete dataset
     # we need to extract the top words with the highest frequency
-    word_features = nltk.FreqDist(all_words)
+    word_features = nltk.FreqDist(all_words).keys()[:2000]
 
     # now working with classifier
     feature_set = [(document_features(d),c) for (d,c) in dataset]
@@ -71,7 +81,7 @@ if __name__ == "__main__":
     print 'Accuracy :',acc
 
     # most informative features
-    classifier.show_most_informative_features(10)
+    classifier.show_most_informative_features(100)
 
     # done
     print "Done!"
